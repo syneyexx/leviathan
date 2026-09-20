7f2166abfc026a2290a1d1108038a998e7b69285
syneyexx <donnyislauw@gmail.com>
2026-09-20 22:05:36 +0200

    restructure: create Data/functions Python namespace

---
+++ Data/functions/__init__.py
@@ -0,0 +1,5 @@
+"""Reusable LEVIATHAN Python functions.
+
+Small stateless helpers and shared function-level utilities belong here.
+Larger stateful features should live in Data.modules instead.
+"""
