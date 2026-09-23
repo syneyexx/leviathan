ab4de33b7ab491d245636bcfbbb34f96f67b24ed
syneyexx <donnyislauw@gmail.com>
2026-09-24 01:20:14 +0200

    style(brain): load fidelity overrides

--- Data/frontend/src/main.tsx
+++ Data/frontend/src/main.tsx
@@ -21,6 +21,7 @@ import "./styles/agents.css";
 import "./styles/media-instagram.css";
 import "./styles/media-facebook.css";
 import "./styles/brain-pages.css";
+import "./styles/brain-fidelity.css";
 import "./styles/pixel-pages.css";
 import "./styles/plugin-runtime-pages.css";
 import "./styles/onderzoek-kennis.css";
